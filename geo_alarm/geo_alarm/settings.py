dd3a8123a759437d4e0368dca97ec84fc7be8e73
Михаил <48497903+akaKAIN@users.noreply.github.com>
2019-09-26 14:43:33 +1000

    Update settings.py

--- geo_alarm/geo_alarm/settings.py
+++ geo_alarm/geo_alarm/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(bb0*eh6*%$fxz@xe+q(l=de@5luchfb)8&3bq@#4@=nz=759l'
+SECRET_KEY = GEO_SECRET
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
