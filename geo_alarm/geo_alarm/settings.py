369ce66d3167b8a9bc8b24ba0aac454f0e7caa3d
akaKAIN <mkain@mail.ru>
2019-09-26 19:21:57 +1000

    checkpoint

--- geo_alarm/geo_alarm/settings.py
+++ geo_alarm/geo_alarm/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = GEO_SECRET
+SECRET_KEY = GEO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -110,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ru'
 
-TIME_ZONE = 'Europe/Khabarovsk'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
